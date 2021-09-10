ba8a99c29311becf382f6d68fbe7ada5d75d79b4
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-10 10:08:39 +0200

    Altering Battlefield and adding ship_count and availability

--- battleship.py
+++ battleship.py
@@ -1,12 +1,29 @@
+import random
+
+
+#ship classes
 class Ship:
+	ship_count = 0
+	ship_available = True
+
 	def __init__(self, length, number):
 		self.length = length
 		self.number = number
+		self.ship_count -= 1
+		if self.ship_count < 1:
+			ship_available = False
+
+
+	def rotate(self, horizontal, vertical):
+		self.horizontal = horizontal
+		self.vertical = vertical
+
 
 class Carrier(Ship):
 	def __init__(self, length, number):
 		self.length = 5
 		self.number = 1
+		super().ship_count
 
 class Battleship(Ship):
 	def __init__(self, length, number):
@@ -28,5 +45,33 @@ class Submarine(Ship):
 		self.length = 1
 		self.number = 2
 
-class Battlefield:
-	pass
+#battlfield 0 = empty 1 is ship
+
+battlefieldA = 	[0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,]
+
+battlefieldB = 	[0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,]
+
+
+
+#ship placement
+
+
+
