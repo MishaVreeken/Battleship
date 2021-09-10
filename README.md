4798a7917c894f6cb739b62a656fc0d43f52ba81
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-10 16:31:40 +0200

    Save for the end of the day

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 battleship
 
-
-raster maken
-battlships maken die er zijn
+battlefield maken en tonen op command zodat je ziet hoe het spel eruit zien
+Random plaatsen voor computer
+    battlships maken die er zijn
 plaatsen en roteren
 (niet op elkaar?)
 schieten maken en dan hit or miss
 winnen en reset
 
-1 of 2 player?
-interface
+
--- battleship.py
+++ battleship.py
@@ -6,14 +6,9 @@ class Ship:
 	ship_available = True
 	ship_quantity = [1, 1, 1, 2, 2]
 
-	def __init__(self, ship_name, length, number):
+	def __init__(self, ship_name, length):
 		self.name = ship_name
 		self.length = length
-		self.number = number
-	
-	def rotate(self, horizontal, vertical):
-		self.horizontal = horizontal
-		self.vertical = vertical
 
 
 # class Carrier(Ship):
@@ -78,7 +73,10 @@ battlefieldBot = 	[0,0,0,0,0,0,0,0,0,0,
 
 
 #ship placement by players give list of ships with quantity and substract it from the pool after placement
+ship = []
+player = input("What is your name?: ")
 
+print("OK! {} Lets start a game of Battleship".format(player))
 while Ship.ship_available == True:
 	x = input("Pick a ship to place: \n"
 		"1 Carrier (Quantity: {Carrier})\n"
@@ -92,47 +90,47 @@ while Ship.ship_available == True:
 			print("No more ships available")
 		else:
 			Ship.ship_quantity[0] -= 1
-			verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")  
-			if verhor == "V" or "v":   #altering the choice doenst work yet
-				verhor == "Vertical"
-			if verhor == "H" or "h":
-				verhor == "Horizontal"	
-			print("Ok lets place it {}".format(verhor))
-
-	if x == "2":
+			ship = Ship("Carrier", 5)
+#			return ship 
+	elif x == "2":
 		if Ship.ship_quantity[1] <= 0:
 			print("No more ships available")
 		else:
 			Ship.ship_quantity[1] -= 1
-			verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")
-			print("Ok lets place it {}".format(verhor))
-
-	if x == "3":
+			ship = Ship("Battleship", 4)
+#			return ship
+	elif x == "3":
 		if Ship.ship_quantity[2] <= 0:
 			print("No more ships available")
 		else:
 			Ship.ship_quantity[2] -= 1
-			verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")
-			print("Ok lets place it {}".format(verhor))
-
-	if x == "4":
+			ship = Ship("Cruiser", 3)
+#			return ship
+	elif x == "4":
 		if Ship.ship_quantity[3] <= 0:
 			print("No more ships available")
 		else:
 			Ship.ship_quantity[3] -= 1
-			verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")
-			print("Ok lets place it {}".format(verhor))
-
-	if x == "5":
+			ship = Ship("Destroyer", 2)
+#			return ship
+	elif x == "5":
 		if Ship.ship_quantity[4] <= 0:
 			print("No more ships available")
 		else:
 			Ship.ship_quantity[4] -= 1
-			verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")
-			print("Ok lets place it {}".format(verhor))
-
+			ship = Ship("Submarine", 1)
+	#		return ship
 	else:
-		print("That wasn't an option")  #this line shouldnt be printed after a choice is made need to fix
+		print("That wasn't an option") 
+
+
+verhor = input("Do you want to place it (H)orizontal or (V)ertical? ")  
+if verhor == "V" or "v":   #altering the choice doenst work yet
+	verhor == "Vertical"
+if verhor == "H" or "h":
+	verhor == "Horizontal"	
+print("Ok lets place it {}".format(verhor))
 
 
 
+place = input("where do you want to place the {}? ".format())
